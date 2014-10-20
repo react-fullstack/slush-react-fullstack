318d0ed4d082d8bf8ffe1a78c1af87dd7de011e3
Hack Reactor Students <accounts@hackreactor.com>
2014-10-20 15:40:53 -0700

    Init Package.json. Generator creates index and gulpfile

---
+++ slushfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var install = require('gulp-install');
+var conflict = require('gulp-conflict');
+var template = require('gulp-template');
+var inquirer = require('inquirer');
+
+gulp.task('default', function (done) {
+  inquirer.prompt([
+    {type: 'input', name: 'name', message: 'Give your app a name', default: gulp.args.join(' ')},
+    {type: 'confirm', name: 'moveon', message: 'Continue?'}
+  ],
+  function (answers){
+    if (!answers.moveon) {
+      return done();
+    }
+    gulp.src(__dirname + '/templates/**')
+      .pipe(template(answers))
+      .pipe(conflict('./'))
+      .pipe(gulp.dest('./'))
+      .pipe(install())
+      .on('end', function() {
+        done();
+      })
+  });
+});
