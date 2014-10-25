0bedd875f555373e007777576d5604820a7dd234
Richard Guan <guan.rich@gmail.com>
2014-10-25 15:13:01 -0700

    bare minimum template created

--- slushfile.js
+++ slushfile.js
@@ -1,8 +1,15 @@
-var gulp = require('gulp');
-var install = require('gulp-install');
-var conflict = require('gulp-conflict');
-var template = require('gulp-template');
-var inquirer = require('inquirer');
+'use strict';
 
+var _ = require('underscore.string');
+var conflict = require('gulp-conflict');
+var gulp = require('gulp');
+var inflection = require('inflection');
+var inquirer = require('inquirer');
+var install = require('gulp-install');
+var mkdirp = require('mkdirp');
+var rename = require('gulp-rename');
+var template = require('gulp-template');
+
+// load generators
 gulp = require('./generators/app')(_, conflict, gulp, inflection, inquirer, install, mkdirp, rename, template);
-gulp = require('./generators/crudModule')(_, conflict, gulp, inflection, inquirer, install, mkdirp, rename, template);
+// gulp = require('./generators/crudModule')(_, conflict, gulp, inflection, inquirer, install, mkdirp, rename, template);
