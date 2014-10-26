509e24f57ec83af1b12c20b52d46fa207ad16737
Richard Guan <guan.rich@gmail.com>
2014-10-26 13:25:34 -0700

    React-component subgenerator works, but does not template

---
+++ generators/reactComponent.js
@@ -0,0 +1,26 @@
+module.exports = function (_, conflict, gulp, inquirer, install, mkdirp, rename, template) {
+	gulp.task('react-component', function (done) {
+		var prompts = [{
+			name: 'componentName',
+			message: 'What is the name of the new component?'
+		}];
+		//Ask
+		inquirer.prompt(prompts, function (answers) {
+			if (!answers) {
+				return done();
+			}
+			answers.slugifiedComponentName = _.slugify(answers.componentName);
+			answers.classifiedComponentName = _.classify(answers.slugifiedComponentName);
+
+			gulp.src(__dirname + '/../templates/react-component/react-component.jsx')
+        .pipe(template(answers, {interpolate: /<\?\?(.+?)\?>/g}))
+        .pipe(rename(answers.classifiedComponentName + '.jsx'))
+        .pipe(conflict('client/app/src/components'))
+        .pipe(gulp.dest('client/app/src/components'))
+        .on('end', function () {
+          done();
+        });	
+		});
+	});
+	return gulp;
+};
--- slushfile.js
+++ slushfile.js
@@ -3,7 +3,6 @@
 var _ = require('underscore.string');
 var conflict = require('gulp-conflict');
 var gulp = require('gulp');
-var inflection = require('inflection');
 var inquirer = require('inquirer');
 var install = require('gulp-install');
 var mkdirp = require('mkdirp');
@@ -11,5 +10,6 @@ var rename = require('gulp-rename');
 var template = require('gulp-template');
 
 // load generators
-gulp = require('./generators/app')(_, conflict, gulp, inflection, inquirer, install, mkdirp, rename, template);
+gulp = require('./generators/app')(_, conflict, gulp, inquirer, install, mkdirp, rename, template);
 // gulp = require('./generators/crudModule')(_, conflict, gulp, inflection, inquirer, install, mkdirp, rename, template);
+gulp = require('./generators/reactComponent')(_, conflict, gulp, inquirer, install, mkdirp, rename, template);
