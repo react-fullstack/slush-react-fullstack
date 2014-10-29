e748dd9bf98e9bee756acd513109bd3614247fc8
Mike <mikeh09@gmail.com>
2014-10-29 16:34:02 -0700

    updated express file to use koa

--- templates/koa-app/server/config/express.js
+++ templates/koa-app/server/config/express.js
@@ -4,56 +4,83 @@
 
 'use strict';
 
-var express = require('express');
+var serve = require('koa-static');
+var session = require('koa-session');//or is it require('koa-session-store')?
+var mongoStore = require('koa-session-mongo');
+var bodyParser = require('koa-bodyparser');
+var passport = require('koa-passport');
+var views = require('koa-views');
+var methodOverride = require('koa-methodoverride');
+
+// var express = require('express');
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+// var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
-var passport = require('passport');
-var session = require('express-session');
-var mongoStore = require('connect-mongo')(session);
+// var passport = require('passport');
+// var session = require('express-session');
+// var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 
 module.exports = function(app) {
-  var env = app.get('env');
+  // var env = app.get('env');
 
-  app.set('views', config.root + '/server/views');
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
+
+  // app.set('views', config.root + '/server/views');
+  // app.engine('html', require('ejs').renderFile);
+  // app.set('view engine', 'html');
+  // app.use(compression());
+  // app.use(bodyParser.urlencoded({ extended: false }));
+  // app.use(bodyParser.json());
+  // app.use(methodOverride());
+  // app.use(cookieParser());
+  // app.use(passport.initialize());
+
+  //using koa-views
+  app.use(views(config.root+'/server/views', {
+    default: 'html'
+  }));
   app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+  app.use(bodyParser());
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
+  app.use(passport.session());
 
   // Persist sessions with mongoStore
   // We need to enable sessions for passport twitter because its an oauth 1.0 strategy
+  // app.use(session({
+  //   secret: config.secrets.session,
+  //   resave: true,
+  //   saveUninitialized: true,
+  //   store: new mongoStore({ mongoose_connection: mongoose.connection })
+  // }));
+
+  app.keys = [config.secrets.session];
   app.use(session({
-    secret: config.secrets.session,
-    resave: true,
-    saveUninitialized: true,
-    store: new mongoStore({ mongoose_connection: mongoose.connection })
+    store: mongoStore.create({
+      mongoose: mongoose.connection;
+    })
   }));
 
-  if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
-  }
+  // if ('production' === env) {
+  //   app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+  //   app.use(serve(path.join(config.root, 'public')));
+  //   app.set('appPath', config.root + '/public');
+  //   app.use(morgan('dev'));
+  // }
 
-  if ('development' === env || 'test' === env) {
+  // if ('development' === env || 'test' === env) {
     // app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
+    app.use(serve(path.join(config.root, '.tmp')));
+    app.use(serve(path.join(config.root, 'client')));
     app.set('appPath', 'client');
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
-  }
+  // }
 };
