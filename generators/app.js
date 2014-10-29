1c925c67a810d069fbec8103cb7ee7da71fbe8d4
Tyler Garrett <tyler@tgrrtt.com>
2014-10-29 16:15:52 -0700

    Generator can generate different stacks

--- generators/app.js
+++ generators/app.js
@@ -1,9 +1,14 @@
 module.exports = function(_, conflict, gulp, inquirer, install, mkdirp, rename, template){
 	gulp.task('default', function (done) {
     var prompts = [{
+			name: 'stack',
+			type: 'list',
+			message: 'What stack would you like to use?',
+			choices: ['Flux', 'Backbone', 'Angular', 'Flux w/ Koa']
+		}, {
       name: 'appName',
       message: 'What would you like to call your application?',
-      default: 'React-Flux-Fullstack'
+      default: 'React-Fullstack'
     }, {
       name: 'appDescription',
       message: 'How would you describe your application?',
@@ -24,15 +29,33 @@ module.exports = function(_, conflict, gulp, inquirer, install, mkdirp, rename, 
       answers.slugifiedAppName = _.slugify(answers.appName);
       answers.humanizedAppName = _.humanize(answers.appName);
       answers.capitalizedAppAuthor = _.capitalize(answers.appAuthor);
-
-      gulp.src(__dirname + '/../templates/app/**')
+			var path;
+			
+			switch (answers.stack) {
+				case 'Flux':
+					path = __dirname + '/../templates/app/**/*';
+					break;
+				case 'Backbone':
+					path = __dirname + '/../templates/app-backbone/**/*';
+					break;
+				case 'Angular':
+					path = __dirname + '/../templates/app-angular/**/*';
+					break;
+				case 'Flux w/ Koa':
+					path = __dirname + '/../templates/app-koa/**/*';
+					break;
+				default:
+					path = __dirname + '/../templates/app/**/*';
+					break;
+			}
+			gulp.src(path)
         .pipe(template(answers, {interpolate: /<\?\?(.+?)\?>/g}))
         .pipe(conflict('./'))
         .pipe(gulp.dest('./'))
         .pipe(install())
         .on('end', function() {
           done();
-        });
+        });	
     });
 	});
 	return gulp;
