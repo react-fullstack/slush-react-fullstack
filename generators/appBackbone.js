f473bdc1ee368b79751a7a9e19fc919bc6bf8c79
concreted <arichuang@gmail.com>
2014-10-29 17:03:54 -0700

    Add backbone gulp task referencing app/backbone

--- generators/appBackbone.js
+++ generators/appBackbone.js
@@ -1,17 +1,17 @@
 module.exports = function(_, conflict, gulp, inquirer, install, mkdirp, rename, template){
-  gulp.task('default', function (done) {
+  gulp.task('backbone', function (done) {
     var prompts = [{
       name: 'appName',
       message: 'What would you like to call your application?',
-      default: 'React-Flux-Fullstack'
+      default: 'React-Flux-Fullstack-Backbone'
     }, {
       name: 'appDescription',
       message: 'How would you describe your application?',
-      default: 'Full-Stack JavaScript with MongoDB, Express, React, Flux and Node.js'
+      default: 'Full-Stack JavaScript with MongoDB, Express, React, Backbone and Node.js'
     }, {
       name: 'appKeywords',
       message: 'How would you describe your application in comma seperated key words?',
-      default: 'MongoDB, Express, React, Flux, Node.js'
+      default: 'MongoDB, Express, React, Backbone, Node.js'
     }, {
       name: 'appAuthor',
       message: 'What is your company/author name?'
@@ -25,7 +25,7 @@ module.exports = function(_, conflict, gulp, inquirer, install, mkdirp, rename, 
       answers.humanizedAppName = _.humanize(answers.appName);
       answers.capitalizedAppAuthor = _.capitalize(answers.appAuthor);
 
-      gulp.src(__dirname + '/../templates/app/**')
+      gulp.src(__dirname + '/../templates/app-backbone/**')
         .pipe(template(answers, {interpolate: /<\?\?(.+?)\?>/g}))
         .pipe(conflict('./'))
         .pipe(gulp.dest('./'))
