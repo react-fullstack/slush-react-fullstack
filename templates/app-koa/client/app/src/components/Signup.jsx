f61c145058b85bdbc4991fbf21346a4af9dfa75a
Tyler Garrett <tyler@tgrrtt.com>
2014-10-31 12:05:38 -0700

    Signup & Login hit DB
    Offers no redirection or any actual use though.

--- templates/app-koa/client/app/src/components/Signup.jsx
+++ templates/app-koa/client/app/src/components/Signup.jsx
@@ -14,11 +14,34 @@ var BANNER = require('./Banner.jsx')
 
 var SIGNUP  = React.createClass({
 
-  handleClick: function(){
-    console.log("at handleClick in Signup")
+  handleSubmit: function(e){
+    e.preventDefault();
+    var name = this.refs.name.getDOMNode().value.trim();
+    var email = this.refs.email.getDOMNode().value.trim();
+    var password = this.refs.password.getDOMNode().value.trim();
+
+    var userData = {
+      name: name,
+      email: email,
+      password: password
+    };
+    
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify(userData),
+      contentType: 'application/json',
+      url: '/api/users/',
+      success: function(item) {
+        console.log('added', item);
+      },
+      failure: function(item) {
+        console.log('failed!');
+      }
+    });
   },
 
   handleInput: function(e) {
+    // make sure data is formatted correctly
     console.log("at handleInput in Signup")
   },
   
@@ -27,29 +50,29 @@ var SIGNUP  = React.createClass({
       <div>
         <div className="container">
         <h1>Signup</h1>
-        <form className="form-horizontal" role="form">
+        <form className="form-horizontal" role="form" onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label for="inputPassword3" className="control-label">Name</label>
               <div className="container">
-                <input type="text" className="form-control" placeholder="Name" onKeyPress={this.handleInput}/>
+                <input type="text" className="form-control" placeholder="Name" ref="name" onKeyPress={this.handleInput}/>
               </div>
             </div>
             <div className="form-group">
               <label for="inputEmail3" className="control-label">Email</label>
               <div className="container">
-                <input type="email" className="form-control" id="inputEmail3" placeholder="Email" onKeyPress={this.handleInput}/>
+                <input type="email" className="form-control" id="inputEmail3" placeholder="Email" ref="email" onKeyPress={this.handleInput}/>
               </div>
             </div>
             <div className="form-group">
               <label for="inputPassword3" className="control-label">Password</label>
               <div className="container">
-                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" onKeyPress={this.handleInput}/>
+                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" ref="password" onKeyPress={this.handleInput}/>
               </div>
             </div>
            
             <div className="form-group">
               <div className="control-button">
-                <button type="submit" className="btn btn-default" onClick={this.handleClick} >Sign up</button>
+                <button type="submit" className="btn btn-default" >Sign up</button>
               </div>
             </div>
           </form>
