b8493e8f684eabc44b0f96f78b3a46fab35fcfde
Nick Litwin <nicklitwin88@gmail.com>
2014-10-30 19:09:49 -0700

    Implement Signup and start Login

--- templates/app/client/app/src/components/Login.jsx
+++ templates/app/client/app/src/components/Login.jsx
@@ -10,8 +10,29 @@ var AppActions = require('../actions/AppActions');
 
 var LOGIN  = React.createClass({
 
-  handleClick: function(){
-    console.log("at handleClick in Signup")
+  handleSubmit: function(e){
+    e.preventDefault();
+
+    var email = this.refs.email.getDOMNode().value.trim();
+    var password = this.refs.password.getDOMNode().value.trim();
+    
+    var userData = {
+      email: email,
+      password: password
+    };
+
+    $.ajax({
+      type: 'GET',
+      data: JSON.stringify(userData),
+      contentType: 'application/json',
+      url: '/api/users/',
+      success: function(item) {
+        console.log('Successfully added: ', item);
+      },
+      failure: function(item) {
+        console.log('Failed! OH NOOOO!');
+      }
+    });
   },
 
   handleInput: function(e) {
@@ -23,17 +44,17 @@ var LOGIN  = React.createClass({
       <div>
         <div className="container">
           <h1>Login</h1>
-            <form className="form-horizontal" role="form">
+            <form className="form-horizontal" role="form" onSubmit={this.handleSubmit} >
               <div className="form-group">
                 <label for="inputEmail3" className="control-label" >Email</label>
                 <div className="container">
-                  <input type="email" className="form-control" id="inputEmail3" placeholder="Email" onKeyPress={this.handleInput}/>
+                  <input type="email" className="form-control" id="inputEmail3" placeholder="Email" ref="email" onKeyPress={this.handleInput}/>
                 </div>
               </div>
               <div className="form-group">
                 <label for="inputPassword3" className="control-label">Password</label>
                 <div className="container">
-                  <input type="password" className="form-control" id="inputPassword3" placeholder="Password" onKeyPress={this.handleInput}/>
+                  <input type="password" className="form-control" id="inputPassword3" placeholder="Password" ref="password" onKeyPress={this.handleInput}/>
                 </div>
               </div>
               <div className="form-group">
@@ -47,7 +68,7 @@ var LOGIN  = React.createClass({
               </div>
               <div className="form-group">
                 <div className="control-button">
-                  <button type="submit" className="btn btn-default" onClick={this.handleClick} >Sign in</button>
+                  <button type="submit" className="btn btn-default" >Sign in</button>
                 </div>
               </div>
             </form>
